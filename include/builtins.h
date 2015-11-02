ae5069da49c1fe350d567961e380dcc5199c05e2
Jackson DeBuhr <jdebuhr@indiana.edu>
2015-11-02 16:07:02 -0500

    Making sure builtins.h is correct; implement factory for LaplaceCOM

--- include/builtins.h
+++ include/builtins.h
@@ -10,13 +10,14 @@ namespace dashmm {
 
 
 //These provide instances of these expansions
-Expansion *laplace_cartesian_expansion();
 Expansion *laplace_COM_expansion();
-Expansion *laplace_spherical_harmonic_expansion(int n_digits);
+Expansion *laplace_sph_expansion(int n_digits);
+Expansion *laplace_sph_exp_expansion(int n_digits);
 
 //These provide instances of these methods
 Method *bh_method(double theta);
-Method *fmm_classic_method();
+Method *fmm_method();
+Method *fmm_exp_method();
 
 
 } // namespace dashmm
