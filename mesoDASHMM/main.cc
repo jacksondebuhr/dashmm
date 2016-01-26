8a0c5ceb1ccbe0207213c96b4c38d1ad7cf97da2
Bo Zhang <zhang416@indiana.edu>
2016-01-26 14:03:24 -0500

    make mesodashmm generate the same points as dashmm

--- mesoDASHMM/main.cc
+++ mesoDASHMM/main.cc
@@ -15,7 +15,9 @@ using namespace std::chrono;
 /// randomly generates particles using number given by the user
 void data_generator(std::vector<dashmm::Source> &sources,
                     std::vector<dashmm::Target> &targets,
-                    std::string datatype) {
+                    std::string datatype, 
+                    std::string test_case) {
+#if 0
   std::mt19937 engine(1);
   std::uniform_real_distribution<double> m_distrib(-1.0, 1.0);
   auto m_roll = std::bind(m_distrib, engine);
@@ -55,6 +57,81 @@ void data_generator(std::vector<dashmm::Source> &sources,
                             sin(theta) * sin(phi),
                             cos(theta)};
     }
+  }
+#endif 
+  srand(123456); 
+  bool use_negative = test_case == std::string{"fmm"}; 
+  if (datatype.compare("cube") == 0) {
+    for (auto i = sources.begin(); i != sources.end(); ++i) {
+      double x = (double) rand() / RAND_MAX; 
+      double y = (double) rand() / RAND_MAX; 
+      double z = (double) rand() / RAND_MAX; 
+      double m = (double) rand() / RAND_MAX + 1.0; 
+      if (use_negative && (rand() % 2)) {
+        m *= -1;
+      }
+      (*i) = dashmm::Source{x, y, z, m}; 
+    }
+
+    for (auto i = targets.begin(); i != targets.end(); ++i) {
+      double x = (double) rand() / RAND_MAX; 
+      double y = (double) rand() / RAND_MAX; 
+      double z = (double) rand() / RAND_MAX; 
+      (*i) = dashmm::Target{x, y, z}; 
+    }    
+  } else if (datatype.compare("sphere") == 0) {
+    for (auto i = sources.begin(); i != sources.end(); ++i) {
+      double r = 1.0; 
+      double ctheta = 2.0 * (double)rand() / RAND_MAX - 1.0; 
+      double stheta = sqrt(1.0 - ctheta * ctheta); 
+      double phi = 2.0 * 3.1415926535 * (double) rand() / RAND_MAX; 
+      double x = r * stheta * cos(phi); 
+      double y = r * stheta * sin(phi); 
+      double z = r * ctheta; 
+      double m = (double) rand() / RAND_MAX + 1.0; 
+      if (use_negative && (rand() % 2)) {
+        m *= -1;
+      }
+      (*i) = dashmm::Source{x, y, z, m}; 
+    }
+
+    for (auto i = targets.begin(); i != targets.end(); ++i) {
+      double r = 1.0; 
+      double ctheta = 2.0 * (double)rand() / RAND_MAX - 1.0; 
+      double stheta = sqrt(1.0 - ctheta * ctheta); 
+      double phi = 2.0 * 3.1415926535 * (double) rand() / RAND_MAX; 
+      double x = r * stheta * cos(phi); 
+      double y = r * stheta * sin(phi); 
+      double z = r * ctheta; 
+      (*i) = dashmm::Target{x, y, z}; 
+    }    
+  } else {
+    // Plummer 
+    int count = sources.size(); 
+    for (auto i = sources.begin(); i != sources.end(); ++i) {
+      double unif = (double)rand() / RAND_MAX; 
+      double r = 1.0 / sqrt(pow(unif, -2.0 / 3.0) - 1); 
+      double ctheta = 2.0 * (double) rand() / RAND_MAX - 1.0; 
+      double stheta = sqrt(1.0 - ctheta * ctheta); 
+      double phi = 2.0 * 3.1415926535 * (double) rand() / RAND_MAX; 
+      double x = r * stheta * cos(phi); 
+      double y = r * stheta * sin(phi); 
+      double z = r * ctheta; 
+      double m = 100.0 / count; 
+      (*i) = dashmm::Source{x, y, z, m}; 
+    }
+
+    for (auto i = targets.begin(); i != targets.end(); ++i) {
+      double unif = (double)rand() / RAND_MAX; 
+      double r = 1.0 / sqrt(pow(unif, -2.0 / 3.0) - 1); 
+      double ctheta = 2.0 * (double) rand() / RAND_MAX - 1.0; 
+      double stheta = sqrt(1.0 - ctheta * ctheta); 
+      double phi = 2.0 * 3.1415926535 * (double) rand() / RAND_MAX; 
+      double x = r * stheta * cos(phi); 
+      double y = r * stheta * sin(phi); 
+      double z = r * ctheta; 
+      (*i) = dashmm::Target{x, y, z}; 
+    }    
   }
 }
 
@@ -153,7 +230,7 @@ int main(int argc, char **argv){
   dashmm::init();
   std::vector<dashmm::Source> sources(nsources);
   std::vector<dashmm::Target> targets(ntargets);
-  data_generator(sources, targets, datatype);
+  data_generator(sources, targets, datatype, method);
 
   dashmm::Expansion *exp_select{nullptr};
   dashmm::Method *method_select{nullptr};
