4c6ea2ab3d4168cefde66df8ab3f6bbde5913af3
Jackson DeBuhr <jdebuhr@indiana.edu>
2016-02-02 13:31:49 -0500

    Templatized case study now is more or less working.

--- case_studies/templateaction/main.cc
+++ case_studies/templateaction/main.cc
@@ -3,41 +3,58 @@
 #include <hpx/hpx.h>
 
 
-template <typename T>
-int accumulate(T *vec, size_t bytes) {
-  size_t count = bytes / sizeof(T);
-  std::cout << "I receive: ";
-  T total{};
-  for (size_t i = 0; i < count; ++i) {
-    total += vec[i];
-    std::cout << vec[i] << " ";
+template <typename T, template <typename> class Op>
+class dashmm {
+ private:
+   hpx_action_t the_action_id;
+
+   static int accumulate_handler(T *vec, size_t bytes) {
+     size_t count = bytes / sizeof(T);
+     std::cout << "I receive: ";
+     T total = Op<T>::initial_value();
+     for (size_t i = 0; i < count; ++i) {
+       Op<T>::reduce(total, vec[i]);
+       std::cout << vec[i] << " ";
+     }
+     std::cout << std::endl << "I compute: " << total << std::endl;
+     hpx_exit(HPX_SUCCESS);
+   }
+
+ public:
+   dashmm() {
+     HPX_REGISTER_ACTION(HPX_DEFAULT, HPX_MARSHALLED, the_action_id,
+                         accumulate_handler, HPX_POINTER, HPX_SIZE_T);
+   }
+
+  int accumulate(T *vec, size_t num) {
+    return hpx_run(&the_action_id, vec, sizeof(T) * num);
   }
-  std::cout << std::endl << "I compute: " << total << std::endl;
-  hpx_exit(HPX_SUCCESS);
-}
-
-
-#define SOME_THING(type)                                        \
-  HPX_ACTION(HPX_DEFAULT, HPX_MARSHALLED,                       \
-           accumulate_##type##_action, accumulate<type>,        \
-           HPX_POINTER, HPX_SIZE_T)                             \
-
-#define ACTION_NAME(type) accumulate_##type##_action
-
-template <typename T>
-struct better_action_name {
-  const hpx_addr_t value = ACTION_NAME(T);
 };
 
 
-SOME_THING(int);
-SOME_THING(double);
-
+template <typename T>
+struct SumOp {
+  static void reduce(T &total, T term) {
+    total += term;
+  }
+  static T initial_value() {
+    return 0;
+  }
+};
 
 template <typename T>
-void did_it_work(T *args, size_t bytes) {
-  hpx_run(&better_action_name<T>::value, args, bytes);
-}
+struct MultOp {
+  static void reduce(T &total, T term) {
+    total *= term;
+  }
+  static T initial_value() {
+    return 1;
+  }
+};
+
+
+dashmm<int, SumOp> dashforint{};
+dashmm<double, MultOp> dashfordouble{};
 
 
 int main(int argc, char **argv) {
@@ -45,13 +62,11 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  //call template action
-  double args[3] = {1.0, 2.2, 3.7};
-  //hpx_run(&ACTION_NAME(double), args, sizeof(double) * 3);
-  did_it_work(args, sizeof(double) * 3);
-
   int argsi[6] = {1, 2, 3, 4, 5, 6};
-  hpx_run(&ACTION_NAME(int), argsi, sizeof(int) * 6);
+  dashforint.accumulate(argsi, 6);
+
+  double args[4] = {2.0, 3.14, 1.2245, 11.0};
+  dashfordouble.accumulate(args, 4);
 
   hpx_finalize();
   return 0;
