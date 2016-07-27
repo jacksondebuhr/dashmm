a1a48e4861d288f9d0df4695957a08350950a51c
Bo Zhang <zhang416@indiana.edu>
2016-07-27 10:17:29 -0400

    change interface for both strong and weak scaling test

--- case_studies/treebuild3/driver.cc
+++ case_studies/treebuild3/driver.cc
@@ -3,18 +3,24 @@
 #include "tree.h"
 
 void usage(std::string exec) {
-  std::cout << "Usage: " << exec << "--datatype=[c/s] " 
-            << "--nsrc-per-rank=num " << "--ntar-per-rank=num "
-            << "--threshold=num " << std::endl; 
+  std::cout << "Usage: " << exec << " --scaling=[w/s] " 
+            << "--datatype=[c/s] " << "--nsrc=num " 
+            << "--ntar=num " << "--threshold=num " 
+            << "--nseed=num " << std::endl; 
+  std::cout << "Note: --nseed=num required if --scaling=s" << std::endl;
 }
 
 int main(int argc, char *argv[]) {
   // default values 
-  char datatype = 'c';
-  int nsrc_per_rank = 1; 
-  int ntar_per_rank = 1; 
-  int threshold = 1; 
- 
+  char scaling = 'w'; // strong scale, 'w' for weak scaling
+  char datatype = 'c'; // cubic point distribution, 's' for sphere distrbution
+  int nsrc = 20; // total number of sources for strong scaling 
+                 // number of sources per rank for weak scaling 
+  int ntar = 20; 
+  int threshold = 1;
+  int nseed = -1; // Number of seeds used for generating input for strong
+                  // scaling test
+
   if (hpx_init(&argc, &argv)) {
     std::cout << "HPX: failed to initialize" << std::endl;
   }
@@ -22,39 +28,59 @@ int main(int argc, char *argv[]) {
   // Parse command line 
   int opt = 0; 
   static struct option long_options[] = {
+    {"scaling", required_argument, 0, 's'}, 
     {"datatype", required_argument, 0, 'd'}, 
-    {"nsrc-per-rank", required_argument, 0, 's'}, 
-    {"ntar-per-rank", required_argument, 0, 't'}, 
+    {"nsrc", required_argument, 0, 'n'}, 
+    {"ntar", required_argument, 0, 'm'}, 
     {"threshold", required_argument, 0, 'l'}, 
+    {"nseed", required_argument, 0, 'r'}, 
     {"help", no_argument, 0, 'h'}, 
     {0, 0, 0, 0}
   }; 
 
   int long_index = 0; 
-  while ((opt = getopt_long(argc, argv, "s:t:l:d:h", 
+  bool valid_arguments = true; 
+  while ((opt = getopt_long(argc, argv, "s:d:n:m:l:r:h", 
                             long_options, &long_index)) != -1) {
     switch (opt) {
     case 's':
-      nsrc_per_rank = atoi(optarg); 
+      scaling = *optarg; 
+      break; 
+    case 'd':
+      datatype = *optarg; 
       break;
-    case 't':
-      ntar_per_rank = atoi(optarg); 
+    case 'n':
+      nsrc = atoi(optarg); 
+      break;
+    case 'm':
+      ntar = atoi(optarg); 
       break;
     case 'l':
       threshold = atoi(optarg); 
       break;
-    case 'd':
-      datatype = *optarg;
+    case 'r':
+      nseed = atoi(optarg); 
       break;
     case 'h':
       usage(std::string{argv[0]});
-      return -1;
+      valid_arguments = false; 
+      break;
     }
   }
 
-  if (hpx_run(&main_action, &nsrc_per_rank, &ntar_per_rank, 
-              &datatype, &threshold)) {
-    std::cout << "Failed to run main action" << std::endl;
+  if (scaling == 's') {
+    // For strong scaling test, @p nseed must be overwritten.
+    if (nseed <= 0) {
+      valid_arguments = false;
+      usage(std::string{argv[0]});
+    }
+  }
+
+  if (valid_arguments) {
+    if (hpx_run(&main_action, &scaling, &datatype, &nsrc, &ntar, 
+                &threshold, &nseed)) {
+      std::cout << "Failed to run main action" << std::endl;
+    }
   }
 
   hpx_finalize(); 
--- case_studies/treebuild3/tree.cc
+++ case_studies/treebuild3/tree.cc
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <iostream>
 #include <algorithm>
+#include <functional>
 #include "tree.h"
 
 double corner_x; 
@@ -29,24 +30,20 @@ int *swap_tar;
 int *bin_tar; 
 int *map_tar; 
 
-void set_points_in_cube(Point *p, int count) {
-  for (int i = 0; i < count; ++i) {
-    double x = 1.0 * rand() / RAND_MAX - 0.5; 
-    double y = 1.0 * rand() / RAND_MAX - 0.5; 
-    double z = 1.0 * rand() / RAND_MAX - 0.5; 
-    p[i] = Point{x, y, z};
-  }
+void set_point_in_cube(Point &p) {
+  double x = 1.0 * rand() / RAND_MAX - 0.5;
+  double y = 1.0 * rand() / RAND_MAX - 0.5; 
+  double z = 1.0 * rand() / RAND_MAX - 0.5; 
+  p = Point{x, y, z}; 
 }
 
-void set_points_on_sphere(Point *p, int count) {
-  for (int i = 0; i < count; ++i) {
-    double theta = 1.0 * rand() / RAND_MAX * M_PI_2; 
-    double phi = 1.0 * rand() / RAND_MAX * M_PI * 2; 
-    double x = sin(theta) * cos(phi); 
-    double y = sin(theta) * sin(phi); 
-    double z = cos(theta); 
-    p[i] = Point{x, y, z}; 
-  }
+void set_point_on_sphere(Point &p) {
+  double theta = 1.0 * rand() / RAND_MAX * M_PI_2; 
+  double phi = 1.0 * rand() / RAND_MAX * M_PI * 2; 
+  double x = sin(theta) * cos(phi); 
+  double y = sin(theta) * sin(phi); 
+  double z = cos(theta); 
+  p = Point{x, y, z}; 
 }
 
 uint64_t split(unsigned k) {  
@@ -110,20 +107,9 @@ int *distribute_points(int num_ranks, const int *global, int len) {
   }
 } 
 
-int generate_input_handler(int nsrc_per_rank, int ntar_per_rank, 
-                           hpx_addr_t sources, hpx_addr_t targets, 
-                           char datatype) {
-  int rank = hpx_get_my_rank(); 
-  hpx_addr_t curr_s = hpx_addr_add(sources, sizeof(ArrayMetaData) * rank, 
-                                   sizeof(ArrayMetaData)); 
-  hpx_addr_t curr_t = hpx_addr_add(targets, sizeof(ArrayMetaData) * rank, 
-                                   sizeof(ArrayMetaData)); 
-
-  ArrayMetaData *meta_s{nullptr}, *meta_t{nullptr}; 
-  if (!hpx_gas_try_pin(curr_s, (void **)&meta_s) || 
-      !hpx_gas_try_pin(curr_t, (void **)&meta_t)) 
-    return HPX_ERROR; 
-
+void generate_weak_scaling_input(ArrayMetaData *meta_s, int nsrc_per_rank,
+                                 ArrayMetaData *meta_t, int ntar_per_rank, 
+                                 char datatype, int seed) {
   meta_s->size = sizeof(Point); 
   meta_s->count = nsrc_per_rank; 
   meta_s->data = new char[sizeof(Point) * nsrc_per_rank](); 
@@ -135,24 +121,130 @@ int generate_input_handler(int nsrc_per_rank, int ntar_per_rank,
   Point *p_s = reinterpret_cast<Point *>(meta_s->data); 
   Point *p_t = reinterpret_cast<Point *>(meta_t->data); 
 
-  // Set the seed of the random number generator. Note that if the seed is set
-  // to 1, the generator is reinitialized to its initial value. For this reason,
-  // the seed is chosen to be 2 + rank here. 
-  srand(rank + 2); 
-  if (datatype == 'c') {
-    set_points_in_cube(p_s, nsrc_per_rank);
-    set_points_in_cube(p_t, ntar_per_rank); 
-  } else {
-    set_points_on_sphere(p_s, nsrc_per_rank); 
-    set_points_on_sphere(p_t, ntar_per_rank);
+  srand(seed); 
+  std::function<void(Point &)> set_point = 
+    (datatype == 'c' ? set_point_in_cube : set_point_on_sphere); 
+
+  for (int i = 0; i < nsrc_per_rank; ++i) 
+    set_point(p_s[i]);
+
+  for (int i = 0; i < ntar_per_rank; ++i) 
+    set_point(p_t[i]);
+}
+
+void generate_strong_scaling_input(ArrayMetaData *meta_s, int nsrc, 
+                                   ArrayMetaData *meta_t, int ntar, 
+                                   char datatype, int rank, int num_ranks, 
+                                   int nseed) {
+  // Divide nsrc and ntar into nseed chunks. Use seed (2 + r) to populate
+  // the points in each chunk, where 0 <= r < nseed
+
+  std::function<void(Point &)> set_point = 
+    (datatype == 'c' ? set_point_in_cube : set_point_on_sphere); 
+
+  int q1, r1, q2, r2, nsrc_curr_rank, s1, s2, ntar_curr_rank, t1, t2; 
+
+  // Generate source points
+  // nsrc = q1 * num_ranks + r1 = q2 * nseed + r2
+  q1 = nsrc / num_ranks; 
+  r1 = nsrc % num_ranks; 
+  q2 = nsrc / nseed; 
+  r2 = nsrc % nseed; 
+  nsrc_curr_rank = (rank < r1 ? q1 + 1 : q1);
+  s1 = (rank < r1 ? (q1 + 1) * rank : (q1 + 1) * r1 + q1 * (rank - r1));
+  s2 = s1 + nsrc_curr_rank - 1; 
+
+  meta_s->size = sizeof(Point); 
+  meta_s->count = nsrc_curr_rank; 
+  meta_s->data = new char[sizeof(Point) * nsrc_curr_rank](); 
+  Point *p_s = reinterpret_cast<Point *>(meta_s->data); 
+
+  for (int r = 0; r < nseed; ++r) {
+    int nsrc_curr_seed = (r < r2 ? q2 + 1 : q2); 
+    int s3 = (r < r2 ? (q2 + 1) * r : (q2 + 1) * r2 + q2 * (r - r2));
+    int s4 = s3 + nsrc_curr_seed - 1; 
+    if (s4 < s1) {
+      continue;
+    } else if (s3 > s2) {
+      break;
+    } else {
+      srand(r + 2); 
+      for (int s = s3; s <= s4; ++s) {
+        if (s >= s1 && s <= s2) 
+          set_point(p_s[s - s1]);
+      }
+    }
   }
 
+  // Generate target points 
+  // ntar = q1 * num_ranks + r1 = q2 * nseed + r2
+  q1 = ntar / num_ranks; 
+  r1 = ntar % num_ranks; 
+  q2 = ntar / nseed; 
+  r2 = ntar % nseed; 
+  ntar_curr_rank = (rank < r1 ? q1 + 1 : q1);
+  t1 = (rank < r1 ? (q1 + 1) * rank : (q1 + 1) * r1 + q1 * (rank - r1));
+  t2 = t1 + ntar_curr_rank - 1; 
+
+  meta_t->size = sizeof(Point); 
+  meta_t->count = ntar_curr_rank; 
+  meta_t->data = new char[sizeof(Point) * ntar_curr_rank](); 
+  Point *p_t = reinterpret_cast<Point *>(meta_t->data); 
+
+  for (int r = 0; r < nseed; ++r) {
+    int ntar_curr_seed = (r < r2 ? q2 + 1 : q2); 
+    int t3 = (r < r2 ? (q2 + 1) * r : (q2 + 1) * r2 + q2 * (r - r2));
+    int t4 = t3 + ntar_curr_seed - 1; 
+    if (t4 < t1) {
+      continue;
+    } else if (t3 > t2) {
+      break;
+    } else {
+      srand(r + 2); 
+      for (int t = t3; t <= t4; ++t) {
+        if (t >= t1 && t <= t2) 
+          set_point(p_t[t - t1]);
+      }
+    }
+  }
+}
+
+int generate_input_handler(char scaling, char datatype, int nsrc, int ntar, 
+                           int nseed, hpx_addr_t sources, hpx_addr_t targets) {
+  int rank = hpx_get_my_rank(); 
+  int num_ranks = hpx_get_num_ranks(); 
+  hpx_addr_t curr_s = hpx_addr_add(sources, sizeof(ArrayMetaData) * rank, 
+                                   sizeof(ArrayMetaData)); 
+  hpx_addr_t curr_t = hpx_addr_add(targets, sizeof(ArrayMetaData) * rank, 
+                                   sizeof(ArrayMetaData)); 
+
+  ArrayMetaData *meta_s{nullptr}, *meta_t{nullptr}; 
+  if (!hpx_gas_try_pin(curr_s, (void **)&meta_s) || 
+      !hpx_gas_try_pin(curr_t, (void **)&meta_t)) 
+    return HPX_ERROR; 
+
+  if (scaling == 'w') {
+    // Generate input for weak scaling test
+    // nsrc and ntar are the number of sources and targets per rank
+
+    // Set the seed of the random number generator. Note that if the seed is set
+    // to 1, the generator is reinitialized to its initial value. For this
+    // reason, the seed is chosen to be 2 + rank here. 
+    generate_weak_scaling_input(meta_s, nsrc, meta_t, ntar, datatype, rank + 2); 
+  } else {
+    // Generate input for strong scaling test
+    // nsrc and ntar are the total number of sources and targets 
+    generate_strong_scaling_input(meta_s, nsrc, meta_t, ntar, datatype, 
+                                  rank, num_ranks, nseed); 
+  }
   hpx_gas_unpin(curr_s); 
   hpx_gas_unpin(curr_t); 
   return HPX_SUCCESS; 
 } 
 HPX_ACTION(HPX_DEFAULT, 0, generate_input_action, generate_input_handler, 
-           HPX_INT, HPX_INT, HPX_ADDR, HPX_ADDR, HPX_CHAR); 
+           HPX_CHAR, HPX_CHAR, HPX_INT, HPX_INT, HPX_INT, HPX_ADDR, HPX_ADDR); 
+           
+
 
 int destroy_input_handler(hpx_addr_t sources, hpx_addr_t targets) {
   int rank = hpx_get_my_rank(); 
@@ -1052,17 +1144,18 @@ int finalize_partition_handler(void *unused, size_t size) {
 HPX_ACTION(HPX_DEFAULT, HPX_MARSHALLED, finalize_partition_action, 
            finalize_partition_handler, HPX_POINTER, HPX_SIZE_T); 
 
-int main_handler(int nsrc_per_rank, int ntar_per_rank, 
-                 char datatype, int threshold) {
+int main_handler(char scaling, char datatype, int nsrc, int ntar, 
+                 int threshold, int nseed) {
   int num_ranks = hpx_get_num_ranks(); 
 
-  // Generate input data 
+  // Generate input data
   hpx_addr_t sources = hpx_gas_calloc_cyclic(num_ranks, 
-                                             sizeof(ArrayMetaData), 0);
+                                             sizeof(ArrayMetaData), 0); 
   hpx_addr_t targets = hpx_gas_calloc_cyclic(num_ranks, 
                                              sizeof(ArrayMetaData), 0); 
-  hpx_bcast_rsync(generate_input_action, &nsrc_per_rank, &ntar_per_rank, 
-                  &sources, &targets, &datatype); 
+
+  hpx_bcast_rsync(generate_input_action, &scaling, &datatype, &nsrc, 
+                  &ntar, &nseed, &sources, &targets); 
 
   // Partition points to create dual tree 
 
@@ -1092,7 +1185,7 @@ int main_handler(int nsrc_per_rank, int ntar_per_rank,
 
   // Choose uniform partition level such that the number of grids is no less
   // than the number of ranks 
-  unif_level = ceil(log(num_ranks) / log(8)); 
+  unif_level = ceil(log(num_ranks) / log(8)) + 1; 
 
   unif_count = hpx_gas_calloc_cyclic(num_ranks, sizeof(hpx_addr_t), 0); 
   unif_done = hpx_gas_calloc_cyclic(num_ranks, sizeof(hpx_addr_t), 0); 
@@ -1127,7 +1220,13 @@ int main_handler(int nsrc_per_rank, int ntar_per_rank,
   hpx_exit(HPX_SUCCESS); 
 }
 HPX_ACTION(HPX_DEFAULT, 0, main_action, main_handler, 
-           HPX_INT, HPX_INT, HPX_CHAR, HPX_INT); 
+           HPX_CHAR, HPX_CHAR, HPX_INT, HPX_INT, HPX_INT, HPX_INT); 
+
+
+
+
+
+
 
 
 void Node::partition(Point *p, int *swap, int *bin, int *map, 
