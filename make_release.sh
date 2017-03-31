f0fc7103083309c6ec706a564e6211186c6a3425
Jackson DeBuhr <jdebuhr@indiana.edu>
2017-03-31 11:53:58 -0400

    Basically ready to go.

--- make_release.sh
+++ make_release.sh
@@ -1,10 +1,16 @@
-VERSION=1.0.0
+VERSION=1.2.0
 BASENAME=dashmm-$VERSION
 FILENAME=$BASENAME.tar
 
 
-echo "Did you remove the comments in evaluator.h?"
-echo "If not, please go remove them now."
+make clean
+cd demo/basic
+make clean
+cd ../stepping
+make clean
+cd ../user
+make clean
+cd ../..
 
 
 mkdir $BASENAME
@@ -13,7 +19,8 @@ cp INSTALL $BASENAME/
 cp Makefile $BASENAME/
 cp README $BASENAME/
 cp LICENSE $BASENAME/
-cp -r doc/ $BASENAME/
+mkdir $BASENAME/doc
+cp doc/DASHMMUserGuide.pdf $BASENAME/doc/
 cp -r demo/ $BASENAME/
 cp -r include/ $BASENAME/
 cp -r src/ $BASENAME/
